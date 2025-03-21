e9bcde995ead9d06273b7b379bc7c37ea0b958ee
Sabu Siyad <hello@ssiyad.com>
2025-03-21 21:44:43 +0530

    fix: term keymap: remove insert mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
 local keymap_set = require('utils.keymap_set')
 
 keymap_set('n', '<Leader>l', ':noh<CR>')
-keymap_set('n', '<Leader>t', ':vs | term<CR> | i')
+keymap_set('n', '<Leader>t', ':vs | term<CR>')
 keymap_set('n', '<Leader>gg', ':tab G<CR>')
