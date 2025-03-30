9bfee11c6f0e14b90d1201cad1ec00b98a60e534
Sabu Siyad <hello@ssiyad.com>
2025-03-31 00:43:41 +0530

    chore: remove most lsp mappings since we have those as default

--- lua/lsp/on_attach.lua
+++ lua/lsp/on_attach.lua
@@ -2,16 +2,10 @@ local lsp = vim.lsp
 local keymap_set = require('utils.keymap_set')
 
 local function f(client, bufnr)
-  keymap_set('n', 'K', lsp.buf.hover)
-  keymap_set('n', '<Leader>gD', lsp.buf.declaration)
-  keymap_set('n', '<Leader>gd', lsp.buf.definition)
-  keymap_set('n', '<Leader>gi', lsp.buf.implementation)
-  keymap_set('n', '<Leader>gr', lsp.buf.references)
-  keymap_set('n', '<Leader>rn', lsp.buf.rename)
-  keymap_set('n', '<Leader>ca', lsp.buf.code_action)
-
   require('lsp.functions.float_diagnostics')
   require('lsp.functions.symbol_highlight')(client, bufnr)
+
+  keymap_set('n', 'grd', lsp.buf.definition)
 end
 
 return f
