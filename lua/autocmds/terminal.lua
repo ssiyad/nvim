b0f7e57909e8a66da647cebabdf0cb2d9800c555
Sabu Siyad <hello@ssiyad.com>
2025-04-01 11:51:41 +0530

    fix: terminal: set number and relativenumber

--- lua/autocmds/terminal.lua
+++ lua/autocmds/terminal.lua
@@ -2,5 +2,7 @@ vim.api.nvim_create_autocmd('TermOpen', {
   desc = 'Open terminal in insert mode',
   callback = function()
     vim.cmd('startinsert!')
+    vim.o.number = true
+    vim.o.relativenumber = true
   end,
 })
