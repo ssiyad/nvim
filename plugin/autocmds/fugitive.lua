2ecdd026de6d1e96b659fbab7efb0674f9ade9df
Sabu Siyad <hello@ssiyad.com>
2025-04-09 17:21:10 +0530

    chore: autocmds: fugitive: changes
    - remove: open fugitive in its own tab
    - remove: open git on right side

--- plugin/autocmds/fugitive.lua
+++ plugin/autocmds/fugitive.lua
@@ -9,23 +9,3 @@ vim.api.nvim_create_autocmd('BufEnter', {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd('BufEnter', {
-  group = group,
-  desc = 'Open fugitive in its own tab.',
-  callback = function()
-    if vim.bo.filetype == 'fugitive' then
-      vim.cmd('silent! wincmd T')
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd('BufEnter', {
-  group = group,
-  desc = 'Open git window in right side.',
-  callback = function()
-    if vim.bo.filetype == 'git' then
-      vim.cmd('silent! wincmd L')
-    end
-  end,
-})
