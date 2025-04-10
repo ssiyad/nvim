d8bee61e267456d99affdc97be0a09e593212bef
Sabu Siyad <hello@ssiyad.com>
2025-04-10 22:35:45 +0530

    chore: change var name

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,8 +1,9 @@
 local statusline = require('mini.statusline')
+
 statusline.setup({
   content = {
     active = function()
-      local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+      local mode, hl_mode = statusline.section_mode({ trunc_width = 120 })
       local diff = statusline.section_diff({ trunc_width = 75 })
       local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
       local filename = statusline.section_filename({ trunc_width = 140 })
@@ -11,13 +12,13 @@ statusline.setup({
       local search = statusline.section_searchcount({ trunc_width = 75 })
 
       return statusline.combine_groups({
-        { hl = mode_hl, strings = { mode } },
+        { hl = hl_mode, strings = { mode } },
         { hl = 'MiniStatuslineDevinfo', strings = { diff, diagnostics } },
         '%<', -- Mark general truncate point
         { hl = 'MiniStatuslineFilename', strings = { filename } },
         '%=', -- End left alignment
         { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-        { hl = mode_hl, strings = { search, location } },
+        { hl = hl_mode, strings = { search, location } },
       })
     end,
   },
