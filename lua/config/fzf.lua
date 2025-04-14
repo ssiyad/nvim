870589d6650f7f86c07a8af93481963ae3e5a56d
Sabu Siyad <hello@ssiyad.com>
2025-04-15 01:12:56 +0530

    chore: fzf-lua: keymaps for toggle-all and accept

--- lua/config/fzf.lua
+++ lua/config/fzf.lua
@@ -2,6 +2,12 @@ local fzf = require('fzf-lua')
 local keymap_set = require('utils.keymap_set')
 
 fzf.setup({
+  keymap = {
+    fzf = {
+      ['ctrl-a'] = 'toggle-all',
+      ['ctrl-q'] = 'accept',
+    },
+  },
   winopts = {
     border = 'solid',
     number = false,
