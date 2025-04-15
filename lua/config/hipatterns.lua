c6429f9d893e68dd1c14959c57b52a6a971d272c
Sabu Siyad <hello@ssiyad.com>
2025-04-15 19:09:34 +0530

    chore: change hipatterns suffix

--- lua/config/hipatterns.lua
+++ lua/config/hipatterns.lua
@@ -1,9 +1,9 @@
 local M = {
   highlighters = {
-    fixme = { pattern = 'Fix:', group = 'MiniHipatternsFixme' },
-    hack = { pattern = 'Hack:', group = 'MiniHipatternsHack' },
-    todo = { pattern = 'Todo:', group = 'MiniHipatternsTodo' },
-    note = { pattern = 'Note:', group = 'MiniHipatternsNote' },
+    fixme = { pattern = 'Fix!', group = 'MiniHipatternsFixme' },
+    hack = { pattern = 'Hack!', group = 'MiniHipatternsHack' },
+    todo = { pattern = 'Todo!', group = 'MiniHipatternsTodo' },
+    note = { pattern = 'Note!', group = 'MiniHipatternsNote' },
   },
 }
 
