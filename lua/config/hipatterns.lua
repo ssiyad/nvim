3b0d93b32aaa808d630b2147e7369530f4afb444
Sabu Siyad <hello@ssiyad.com>
2025-04-24 21:18:11 +0530

    chore: patterns: add rust todo and unimplemented

--- lua/config/hipatterns.lua
+++ lua/config/hipatterns.lua
@@ -4,6 +4,8 @@ local M = {
     hack = { pattern = 'Hack!', group = 'MiniHipatternsHack' },
     todo = { pattern = 'Todo!', group = 'MiniHipatternsTodo' },
     note = { pattern = 'Note!', group = 'MiniHipatternsNote' },
+    rust_todo = { pattern = 'todo!', group = 'MiniHipatternsTodo' },
+    rust_unimplemented = { pattern = 'unimplemented!', group = 'MiniHipatternsHack' },
   },
 }
 
