155cbca50333842bb7815edea58fbaa2d164a9b5
Sabu Siyad <hello@ssiyad.com>
2025-06-16 20:26:19 +0530

    chore: hipatterns: add bug and refactor

--- lua/config/hipatterns.lua
+++ lua/config/hipatterns.lua
@@ -4,6 +4,8 @@ local M = {
     hack = { pattern = 'Hack!', group = 'MiniHipatternsHack' },
     todo = { pattern = 'Todo!', group = 'MiniHipatternsTodo' },
     note = { pattern = 'Note!', group = 'MiniHipatternsNote' },
+    bug = { pattern = 'Bug!', group = 'MiniHipatternsFixme' },
+    refactor = { pattern = 'Refactor!', group = 'MiniHipatternsHack' },
     rust_todo = { pattern = 'todo!', group = 'MiniHipatternsTodo' },
     rust_unimplemented = { pattern = 'unimplemented!', group = 'MiniHipatternsHack' },
   },
