c246b93d28df3cabd7eb6f485d7b5d1ab359a4dc
Sabu Siyad <hello@ssiyad.com>
2025-06-17 09:51:01 +0530

    fix: avante: use fzf_lua

--- lua/config/avante.lua
+++ lua/config/avante.lua
@@ -3,7 +3,15 @@ local M = {
   providers = {
     copilot = {
       model = 'claude-3.7-sonnet',
+      extra_request_body = {
+        timeout = 30000,
+        temperature = 0.75,
+        max_completion_tokens = 8192,
+      },
     },
+  },
+  selector = {
+    provider = 'fzf_lua',
   },
 }
 
