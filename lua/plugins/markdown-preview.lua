d4953f3308d4d16a42701a0acd67419beca9a744
Sabu Siyad <hello@ssiyad.com>
2025-06-19 22:09:07 +0530

    chore: remove markdown-preview

--- lua/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
--- markdown preview plugin for (neo)vim
--- https://github.com/iamcco/markdown-preview.nvim
-
-return {
-  'iamcco/markdown-preview.nvim',
-  build = function()
-    vim.fn['mkdp#util#install']()
-  end,
-}
