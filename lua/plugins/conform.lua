2dbe35e35333578f511159ab9fb295d68b84fc4e
Sabu Siyad <hello@ssiyad.com>
2025-07-05 10:53:07 +0530

    feat: formatter for html and htmldjango

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,8 +4,43 @@
 return {
   'stevearc/conform.nvim',
   config = function()
-    require('config.conform')
+    local js = {
+      'eslint_d',
+      'prettier',
+      'rustywind',
+    }
+
+    local html = {
+      'djlint',
+      'rustywind',
+    }
+
+    require('conform').setup({
+      formatters = {
+        djlint = {
+          prepend_args = { '--indent', 2 },
+        },
+      },
+      formatters_by_ft = {
+        html = html,
+        htmldjango = html,
+        lua = {
+          'stylua',
+        },
+        python = {
+          'ruff_fix',
+          'ruff_format',
+          'ruff_organize_imports',
+        },
+        javascript = js,
+        typescript = js,
+        typescriptreact = js,
+        vue = js,
+      },
+      format_after_save = {
+        lsp_fallback = true,
+      },
+    })
   end,
   event = 'VeryLazy',
 }
-
