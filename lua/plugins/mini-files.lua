6560066f87994bb321816997bf3f69e8a97f927e
Sabu Siyad <hello@ssiyad.com>
2025-08-22 22:49:55 +0530

    feat: files: open in split and tab

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -12,5 +12,28 @@ return {
     keymap_set('n', '<Leader>o', function()
       files.open()
     end)
+
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        local cur_target = files.get_explorer_state().target_window
+        local new_target = vim.api.nvim_win_call(cur_target, function()
+          vim.cmd(direction .. ' split')
+          return vim.api.nvim_get_current_win()
+        end)
+        files.set_target_window(new_target)
+      end
+      local desc = 'Split ' .. direction
+      vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        map_split(buf_id, '<C-s>', 'belowright horizontal')
+        map_split(buf_id, '<C-v>', 'belowright vertical')
+        map_split(buf_id, '<C-t>', 'tab')
+      end,
+    })
   end,
 }
