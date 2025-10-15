7d792da6a5e5a5ae9f9b1e1999b0e3217ac916d9
Sabu Siyad <hello@ssiyad.com>
2025-10-15 13:05:44 +0530

    chore: change highlight patterns

--- lua/plugins/mini-hipatterns.lua
+++ lua/plugins/mini-hipatterns.lua
@@ -5,12 +5,12 @@ return {
 
     hipatterns.setup({
       highlighters = {
-        fixme = { pattern = 'Fix!', group = 'MiniHipatternsFixme' },
-        hack = { pattern = 'Hack!', group = 'MiniHipatternsHack' },
-        todo = { pattern = 'Todo!', group = 'MiniHipatternsTodo' },
-        note = { pattern = 'Note!', group = 'MiniHipatternsNote' },
-        bug = { pattern = 'Bug!', group = 'MiniHipatternsFixme' },
-        refactor = { pattern = 'Refactor!', group = 'MiniHipatternsHack' },
+        fixme = { pattern = 'FIX:', group = 'MiniHipatternsFixme' },
+        hack = { pattern = 'HACK:', group = 'MiniHipatternsHack' },
+        todo = { pattern = 'TODO:', group = 'MiniHipatternsTodo' },
+        note = { pattern = 'NOTE:', group = 'MiniHipatternsNote' },
+        bug = { pattern = 'BUG:', group = 'MiniHipatternsFixme' },
+        refactor = { pattern = 'REFACTOR:', group = 'MiniHipatternsHack' },
         rust_todo = { pattern = 'todo!', group = 'MiniHipatternsTodo' },
         rust_unimplemented = { pattern = 'unimplemented!', group = 'MiniHipatternsHack' },
       },
