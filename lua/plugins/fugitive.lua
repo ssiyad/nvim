2da6aa3cda840d74401fe5b6eeb52d5f56bc350a
Sabu Siyad <hello@ssiyad.com>
2025-11-15 23:12:55 +0530

    chore(fugitive): move autocmds to package config

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -9,5 +9,14 @@ return {
     'tpope/vim-rhubarb',
   },
   event = 'VeryLazy',
+  config = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      desc = 'Open git commit edit in insert mode.',
+      callback = function()
+        if vim.bo.filetype == 'gitcommit' then
+          vim.cmd('startinsert!')
+        end
+      end,
+    })
+  end,
 }
-
