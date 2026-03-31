886775130702a2e8713f67f4829104f9b7c4a5e9
Sabu Siyad <hello@ssiyad.com>
2026-03-31 22:35:10 +0530

    chore: move settings to init

--- init.lua
+++ init.lua
@@ -1,3 +1,34 @@
+vim.g.mapleader = ' '
+
+vim.opt.autoindent = true
+vim.opt.autoread = true
+vim.opt.cmdheight = 0
+vim.opt.cursorline = true
+vim.opt.ff = 'unix'
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.laststatus = 3
+vim.opt.mouse = ''
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 0
+vim.opt.shortmess:append('WIFS')
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.spell = true
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.updatetime = 750
+vim.opt.winborder = 'solid'
+
+-- Enable diagnostic message for current line only.
+vim.diagnostic.config({
+  virtual_text = {
+    current_line = true,
+  },
+})
+
 local git_repo = 'git@github.com:folke/lazy.nvim.git'
 local install_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
@@ -13,6 +44,5 @@ if not (vim.uv or vim.loop).fs_stat(install_path) then
 end
 
 vim.opt.runtimepath:prepend(install_path)
-require('settings')
 require('keymaps')
 require('lazy').setup('plugins')
